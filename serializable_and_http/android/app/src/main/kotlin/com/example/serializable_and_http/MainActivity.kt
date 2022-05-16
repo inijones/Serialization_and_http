ca7cd771f163764796088ec1338aefdd1589ac07
Ini Jones <jonesinim@gmail.com>
2022-05-16 14:28:04 +0100

    Upgraded the Flutter Create File

---
+++ serializable_and_http/android/app/src/main/kotlin/com/example/serializable_and_http/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.serializable_and_http
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ serializable_and_http/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:serializable_and_http/main_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Recipes',
+      theme: ThemeData(
+        brightness: Brightness.light,
+        primaryColor: Colors.white,
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const MainScreen(),
+    );
+  }
+}
+
