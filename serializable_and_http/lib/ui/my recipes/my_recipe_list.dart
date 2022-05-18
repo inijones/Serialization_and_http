fd625e229eaa138a04ffa672c3e3dc1b0ea3c6bd
Ini Jones <jonesinim@gmail.com>
2022-05-18 08:56:15 +0100

    Integrated the Endpoint into the Home Page Successful

--- serializable_and_http/lib/ui/my recipes/my_recipe_list.dart
+++ serializable_and_http/lib/ui/my recipes/my_recipe_list.dart
@@ -1,1 +1,71 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+
+class MyRecipeList extends StatefulWidget {
+  const MyRecipeList({Key? key}) : super(key: key);
+
+  @override
+  State<MyRecipeList> createState() => _MyRecipeListState();
+}
+
+class _MyRecipeListState extends State<MyRecipeList> {
+  List<String> recipes = [];
+
+  @override
+  void initState() {
+    super.initState();
+    recipes = <String>[];
+  }
+
+  @override
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: _buildRecipeList(context),
+    );
+  }
+
+  Widget _buildRecipeList(BuildContext context) {
+    return ListView.builder(itemBuilder: (BuildContext context, int index) {
+      return SizedBox(
+        height: 100,
+        child: Slidable(
+          endActionPane: ActionPane(
+            motion: ScrollMotion(),
+            children: [
+              SlidableAction(
+                onPressed: (context) {},
+                label: "Delete",
+                foregroundColor: Colors.black,
+              ),
+            ],
+          ),
+          child: Card(
+            elevation: 1.0,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+            color: Colors.white,
+            child: Align(
+              alignment: Alignment.center,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ListTile(
+                  leading: CachedNetworkImage(
+                    imageUrl:
+                        'http://www.seriouseats.com/recipes/2011/12/chicken-vesuvio-recipe.html',
+                    height: 120,
+                    width: 60,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      );
+    });
+  }
+}
